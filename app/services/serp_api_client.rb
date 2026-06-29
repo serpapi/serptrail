63d060bb0dea9eb8f22f34aee132685576ddf250
David Sojevic <david@sojevic.com>
2026-06-29 10:37:52 +1000

    Fix hostname matching and support subdomain matching

--- app/services/serp_api_client.rb
+++ app/services/serp_api_client.rb
@@ -11,13 +11,13 @@ class SerpApiClient
     client&.close
   end
 
-  def extract_position(results, domain)
+  def extract_position(results, domain, match_subdomains: false)
     organic = results[:organic_results] || []
-    match = organic.each_with_index.find { |result, _| result[:link]&.include?(domain) }
+    match = organic.each_with_index.find { |result, _| link_matches_domain?(result[:link], domain, match_subdomains: match_subdomains) }
 
     ao = results[:ai_overview]
     ao_sources = ao&.dig(:sources) || []
-    ao_source = ao_sources.find { |source| source[:link]&.include?(domain) }
+    ao_source = ao_sources.find { |source| link_matches_domain?(source[:link], domain, match_subdomains: match_subdomains) }
 
     position, url = if match
       result, = match
@@ -35,11 +35,28 @@ class SerpApiClient
     }
   end
 
-  def check_position(query, domain, location:)
+  def check_position(query, domain, location:, match_subdomains: false)
     results = search(query, location: location)
-    extract_position(results, domain).merge(
+    extract_position(results, domain, match_subdomains: match_subdomains).merge(
       serpapi_search_id: results.dig(:search_metadata, :id),
       raw_response: results.to_json
     )
+  end
+
+  def link_matches_domain?(link, domain, match_subdomains: false)
+    link_host = extract_hostname(link)
+    domain = extract_hostname(domain)
+
+    return false if link_host.blank? || domain.blank?
+
+    link_host == domain || (match_subdomains && link_host.end_with?(".#{domain}"))
+  end
+
+  def extract_hostname(value)
+    hostname = (value.to_s.include?("/") ? URI.parse(value)&.hostname : value).to_s
+
+    hostname.strip.downcase.delete_prefix("www.").delete_suffix(".")
+  rescue URI::InvalidURIError
+    nil
   end
 end
