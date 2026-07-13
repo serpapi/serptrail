e05964fccd74af2ab8cb2199437c2268b8cc19b2
Josef Strzibny <strzibny@strzibny.name>
2026-07-13 14:03:29 +0200

    Simplify seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,7 @@
 if Rails.env.development?
-  require "active_record/fixtures"
-  ActiveRecord::FixtureSet.reset_cache
-  ActiveRecord::FixtureSet.create_fixtures(
-    Rails.root.join("test/fixtures"),
-    %w[sites keywords keyword_targets search_runs search_run_pages checks tenants]
-  )
+  require "rake"
+  Rails.application.load_tasks unless Rake::Task.task_defined?("db:fixtures:load")
+  Rake::Task["db:fixtures:load"].invoke
 else
-  Tenant.find_or_create_by!(id: 1) do |t|
-    t.serpapi_key = ENV.fetch("SERPAPI_API_KEY", "")
-    t.openai_api_key = ENV.fetch("OPENAI_API_KEY", "")
-  end
+  Tenant.find_or_create_by!(id: 1)
 end
